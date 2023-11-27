f0327d62893a97e4ede34a220a256d789f6c0ce5
U <U@U>
2023-11-27 22:14:40 +0300

    Добавьте файлы проекта.

---
+++ Wave/Wave.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
+int main()
+{
+    string Message = "^";
+    for (int j = 0; j < 8; j++)
+    {
+        //нисходящая 
+        for (int a = 10; a > 1; a--)
+        {
+            string NegMultipleSpace (10 - a, ' ');
+            string MultipleSpace (a, ' ');
+            cout << ("." + MultipleSpace + Message + NegMultipleSpace + NegMultipleSpace + Message) << endl;
+            Sleep(40);
+        }
+        return 0;
+    }
+}
